77a57c9d67160eca0cf7fe00f9b467e826782b2b
unknown <ikrameeidrissi@gmail.com>
2023-10-24 20:25:22 +0000

    added models

---
+++ models/Category.js
@@ -0,0 +1,11 @@
+const mongoose=require('mongoose');
+const categoryShema=new mongoose.Schema({
+    name:{
+        type:String,
+        require:true,
+        maxLength:32,
+        trim:true
+
+    }
+},{timestamps:true})
+module.exports=mongoose.model('Category',categoryShema);
